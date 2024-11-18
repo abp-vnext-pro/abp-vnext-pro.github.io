7386ce1f0b6d2d8bd139c32eaad94388ed569629
hanpaopao <510423039@qq.com>
2024-11-18 11:16:44 +0800

    f

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -7,7 +7,7 @@ type NavData = {
 
 export const NAV_DATA: NavData[] = [
   {
-    title: '常用工具',
+    title: '常用工具111',
     items: [
       {
         icon: 'https://caniuse.com/img/favicon-128.png',
