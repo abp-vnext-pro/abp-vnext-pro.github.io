c5336c190d8784ea10d30d1370b691f034404c5b
hanpaopao <510423039@qq.com>
2024-11-19 11:04:54 +0800

    feat: 完善文档

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -5,6 +5,9 @@ export const nav: DefaultTheme.Config['nav'] = [
   { text: '文档', link: '/guide/quick-start' },
   { text: '演示Vben28', link: 'http://182.43.18.151:44318/' },
   { text: '演示Vben5', link: 'http://182.43.18.151:44320/' },
+  { text: '✨赞助', link: '/about/donate' },
+  { text: '👨‍👦‍👦社区', link: '/about/community' },
+  { text: '🦄定制开发', link: '/about/dev' },
   // {
   //   text: '技术支持',
   //   link: 'https://notes.fe-mm.com',
